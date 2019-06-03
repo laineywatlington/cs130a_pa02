846af39011ddc5a143525f3eb1966678f404acf3
laineywatlington <lainey.watlington@gmail.com>
2019-06-03 11:30:51 -0700

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -1,1 +1,15 @@
 #include "Tree.h"
+#include <iostream>
+
+//finds max of two ints
+int maximum(int x, int y){
+  if(x < y){
+    return y;
+  }
+  else{
+    return x;
+  }
+}
+
+//finds min of two ints
+int 
