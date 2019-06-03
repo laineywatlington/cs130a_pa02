f97f5cfa3ffba7d2bdd689f4de26851cff8685fc
laineywatlington <lainey.watlington@gmail.com>
2019-06-03 12:02:06 -0700

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -23,10 +23,34 @@ int minimum(int x, int y){
 
 //get height of tree rooted at node
 int getHeight(Node* node){
+  //if node doesnt exist
   if(node == NULL){
     return 0;
   }
   else{
     return (node->height);
+  }
+}
+
+//create a new node with desired key 
+//node will be a leaf
+Node* newNode(int key){
+  Node* node = new Node;
+  node -> key = key;
+  node -> leftchild = NULL;
+  node -> rightchild = NULL;
+  node -> height = 1;
+  return node;
+}
+
+//get height difference for left and right subtrees of node
+int balanceFactor(Node* node){
+  if(node == NULL){
+    return 0;
+  }
+  else{
+    int leftHeight = height(node -> leftchild);
+    int rightHeight = height(node -> rightchild);
+    return (leftHeight - rightHeight);
   }
 }
