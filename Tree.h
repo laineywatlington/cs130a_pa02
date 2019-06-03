f6598f2d22c62528f07b54c9120e72295e4b5daf
laineywatlington <lainey.watlington@gmail.com>
2019-06-03 10:44:18 -0700

    define node class

--- Tree.h
+++ Tree.h
@@ -1,4 +1,17 @@
 #ifndef TREE_H
 #define TREE_H
 
+using namespace std;
+
+//create node class, this is what we will insert into tree
+class Node{
+  public:
+  int key;
+  int height;
+  Node* leftchild;
+  Node* rightchild;
+};
+
+//now define functions here
+
 #endif
