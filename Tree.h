b6e1528b585c89e385535c8ae364dbc66898c14d
laineywatlington <lainey.watlington@gmail.com>
2019-06-04 14:19:00 -0700

    Update Tree.h

--- Tree.h
+++ Tree.h
@@ -16,6 +16,20 @@ class Node{
 int maximum(int x, int y);
 int minimum(int x, int y);
 int getHeight(Node* node);
+void preOrder(Node* root);
+//building tree
 Node* newNode(int key);
+Node* rotateRight(Node* node);
+Node* rotateLeft(Node* node);
+Node* insert(Node* node, int key, int* rotations, int* visits);
+
+//checks
 int balanceFactor(Node* node);
+int lookup(Node* root, int searchFor, int* visits);
+//printing/parsing
+void printTree(Node* root, int* c);
+void printLL
+void printRR
+void printLR
+void printRL
 #endif
