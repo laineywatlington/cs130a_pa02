3028b466f39210b223288d1373ce6c4998c1ba9f
laineywatlington <lainey.watlington@gmail.com>
2019-06-05 16:54:03 -0700

    Update avl.cpp

--- avl.cpp
+++ avl.cpp
@@ -1,1 +1,21 @@
 #include "Tree.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+//first, extract all ints from input files and store in vector
+vector<int> int_vector(string array){
+  stringstream input;
+  vector<int> vec;
+  inout << array;
+  string temp; 
+  int number; 
+  while (!input.eof()){ 
+    input >> temp;
+    if (stringstream(temp) >> number){ 
+      vec.push_back(number);
+    }
+    temp = "";
+  }
+  return vec;
+}
